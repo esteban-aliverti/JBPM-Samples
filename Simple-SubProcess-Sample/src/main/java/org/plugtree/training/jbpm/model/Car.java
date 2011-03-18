671685e4a648148ad0e1cc49c17477240d8194e8
Esteban <esteban.aliverti@gmail.com>
2011-03-17 21:37:06 -0300

    - Dynamic SubProcesses Selection samples added

---
+++ Simple-SubProcess-Sample/src/main/java/org/plugtree/training/jbpm/model/Car.java
@@ -0,0 +1,27 @@
+package org.plugtree.training.jbpm.model;
+
+/**
+ *
+ * @author esteban
+ */
+public class Car {
+    private String brand;
+    private String model;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+}
---
+++ Simple-SubProcess-Sample/src/main/java/org/plugtree/training/jbpm/model/Person.java
@@ -0,0 +1,18 @@
+package org.plugtree.training.jbpm.model;
+
+/**
+ *
+ * @author esteban
+ */
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
